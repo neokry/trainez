fb3068c707b56f7f7495779577e9e52173aa241d
Patrick Genevich <patrickgenevich@gmail.com>
2021-01-08 14:31:10 -0800

    Fixes bug where signin was not working

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,7 +1,19 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import Signup from "../components/signup";
+import { useStream } from "../hooks/useStream";
 
 export default function Login() {
+    const stream = useStream();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (stream.currentUser) {
+            router.push("/");
+        }
+    }, [stream.currentUser]);
+
     return (
         <div className="md:flex md:justify-start">
             <div className="mt-24 md:mt-0 md:w-1/2 flex justify-around">
